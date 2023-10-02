54d86ab4c44cd31a4613ebf6776d97873d3a1f01
Eva Marko <valentova.eva92@gmail.com>
2023-10-02 12:06:05 +0200

    MainController with one method added
    Added MainController with textAndPicture method which returns ModelAndView consisting of picture and text attributes.

---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -0,0 +1,36 @@
+package cz.czechitas.java2webapps.ukol2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Controller
+public class MainController {
+    private final Random random = new Random();
+
+    private static List<String> readAllLines(String resource)throws IOException {
+        ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+        try(InputStream inputStream=classLoader.getResourceAsStream(resource);
+            BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+            return reader.lines().collect(Collectors.toList());
+        }
+    }
+
+    @GetMapping("/")
+    public ModelAndView textAndPicture() throws IOException {
+        int randomNumber = random.nextInt(8) + 1;
+        ModelAndView result = new ModelAndView("index");
+        result.addObject("picture", String.format("/images/picture%d.jpg", randomNumber));
+        result.addObject("text", readAllLines("citaty.txt").get(randomNumber - 1));
+        return result;
+    }
+}
